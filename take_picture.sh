246e18d811de6967f11a04056a14ce977370538b
swsdn <krzysiek.kol@gmail.com>
2014-07-21 23:22:41 +0200

    changed destination folder

--- take_picture.sh
+++ take_picture.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/opt/vc/bin/raspistill -rot 180 -n --timelapse 300000 --timeout 86400000 -q 8 -o photos/`date +%Y%m%d-%H%M%S`-%04d.jpg
+/opt/vc/bin/raspistill -rot 180 -n --timelapse 300000 --timeout 86400000 -q 8 -o ~/photos/`date +%Y%m%d-%H%M%S`-%04d.jpg
 
 #night photos, not working :(
 #/opt/vc/bin/raspistill -t 240000 -ss 24000000 -n -q 100 --ISO 100 -o photos/`date +%Y%m%d-%H%M%S`-%04d.jpg
