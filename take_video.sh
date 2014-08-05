b1fd5606c053cf6b8ef483f8951cc16cf4e7219e
swsdn <krzysiek.kol@gmail.com>
2014-08-05 23:40:36 +0200

    take video with nice frame and bit rate

--- take_video.sh
+++ take_video.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/opt/vc/bin/raspivid --rotation 180 --width 1440 --height 1080 --verbose --nopreview --timeout 3600000 --framerate 5 --bitrate 1000000 --segment 10000 --wrap 180 --output ~/videos/vid_`date +%Y%m%d-%H%M%S`-%04d.h264
+/opt/vc/bin/raspivid --rotation 180 --width 1440 --height 1080 --verbose --nopreview --timeout 3600000 --framerate 30 --bitrate 3500000 --segment 10000 --wrap 180 --output ~/videos/vid_`date +%Y%m%d-%H%M%S`-%04d.h264
 
 #`date +%Y%m%d-%H%M%S`.jpg
 
