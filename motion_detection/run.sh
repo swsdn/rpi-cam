590061902207243a08661e1b132d50db20794d2d
swsdn <krzysiek.kol@gmail.com>
2015-06-09 00:16:46 +0200

    deleted java project

--- motion_detection/run.sh
+++
@@ -1,3 +0,0 @@
-mvn clean dependency:copy-dependencies package
-
-java -cp "target/motion-0.0.1-SNAPSHOT.jar;/target/dependency/*" com.swsdn.rpi.motion.Camera
--- motion_detection/src/main/java/com/swsdn/rpi/motion/model/Picture.java
+++
@@ -1,7 +0,0 @@
-package com.swsdn.rpi.motion.model;
-
-/**
- * Created by Krzysiek on 2014-12-02.
- */
-public class Picture {
-}
